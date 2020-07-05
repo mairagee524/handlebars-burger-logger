56ca8017461a1304d11c2243e508a43a4e170ef7
mairagee524 <garciamaira524@yahoo.com>
2020-07-05 11:41:37 -0700

    modified code in js files to make app work

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(50) NOT NULL,
-	-- eaten BOOLEAN DEFAULT false,
+	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (name) 
 VALUES 
-    ('Sample Burger 1');
+    ('Sample Burger 1', 1);
