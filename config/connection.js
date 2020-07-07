1538c9061db548b0b6f792f4422167a459f5183e
mairagee524 <garciamaira524@yahoo.com>
2020-07-06 18:13:03 -0700

    edit JAWSDB MYSQL

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,21 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "blinky182",
+        database: "burgers_db"
+      });
+}
+
+
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
