579e3e75b9c9889fb398c201bc55df03194c3892
mairagee524 <garciamaira524@yahoo.com>
2020-07-06 18:15:45 -0700

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -15,15 +15,6 @@ if(process.env.JAWSDB_URL) {
       });
 }
 
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "blinky182",
-  database: "burgers_db"
-});
-
 // Make connection.
 connection.connect(err => {
   if (err) {
